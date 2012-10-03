20ef0a68bd769932de6780b1c96728d4ef265189
Jedrek Siema <jenczuen@gmail.com>
2012-10-03 23:10:28 +0200

    about triangle project 2

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,6 +14,19 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+	if a == 0 or b == 0 or c == 0 
+		raise TriangleError, "Length of an edge can't be 0"
+	end
+	if a < 0 or b < 0 or c < 0 
+		raise TriangleError, "Length of an edge can't be a negative"
+	end
+
+	edges = [a,b,c]
+	edges.sort!
+	unless edges[0] + edges[1] > edges[2]
+		raise TriangleError, "Edges don't make a triangle"
+	end
+
 	if a == b 
 		if a == c 
 			:equilateral
